5a4158e31b0d4379ce91c5367be98ea1caea588d
Bryan Ruiz <bmr1919@hotmail.com>
2020-01-19 22:54:02 -0600

    Proyecto Base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Bienvenidos</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Estas Logueado!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+Route::get('/', 'TestController@welcome');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
